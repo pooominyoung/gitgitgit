221fc99161ea9994e9eb2179fd1cfc60dffecf4f
pooominyoung <77039272+pooominyoung@users.noreply.github.com>
2023-04-19 19:38:19 +0900

    testsite

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/IMG_2849.jpg') {
+        myImage.setAttribute
+('src','images/IMG_2848.jpg');
+    } else{
+      myImage.setAttribute('src','images/IMG_2849.jpg');
+    }
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter your name');
+    if(!myName || myName === null){
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Nice to meet you ' +myName+' BaBooo';
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Long time no see ' +storedName + ' BaBooo!';
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
